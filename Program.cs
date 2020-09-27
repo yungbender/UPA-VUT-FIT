70d4ffd8e562d5c0d9821b8a8b7dd502f415afbe
Tomáš Sasák <tsasak@redhat.com>
2020-09-27 21:44:22 +0200

    Add base files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UPA_VUT_FIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+        }
+    }
+}
