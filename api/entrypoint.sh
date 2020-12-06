f5c2add0210b1f1825c3e0a269fbb59cee5f5d8a
TomasVenkrbec <xvenkr01@stud.fit.vutbr.cz>
2020-12-06 18:04:22 +0100

    Fixed startup order

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+while true;
+do
+    sleep 1
+    echo "Trying to contact postgres..."
+    pg_isready -h upa-postgres -U $SQL_USERNAME -d $SQL_DBNAME &>/dev/null
+    if [ "$?" == 0 ]
+    then
+        break
+    fi
+done;
+
 exec api
